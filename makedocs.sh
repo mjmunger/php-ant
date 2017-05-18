d23a9df0958c8c1bc97884198381b1d31da42b49
Michael Munger <michael@highpoweredhelp.com>
2017-05-18 09:24:30 -0400

    Updated makedocs.sh and included / excluded files

--- makedocs.sh
+++ makedocs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Generates PHPDoc documentation for this PHP-Ant Installation.
 rm -vfr "./docs"
-phpdoc -d "." -t "./docs" \
+phpdoc -d "./includes/" -t "./docs" \
+--ignore="includes/vendor/*" \
+--ignore="includes/libs/*" \
 --ignore="includes/classes/pImage.class.php" \
 --ignore="includes/classes/pScatter.class.php" \
 --ignore="includes/classes/pStock.class.php" \
 --ignore="includes/classes/pSpring.class.php" \
---ignore="www/includes/classes/pBarcode128.class.php" \
+--ignore="includes/classes/pBarcode128.class.php" \
 --ignore="includes/classes/pBubble.class.php" \
 --ignore="includes/classes/pRadar.class.php" \
 --ignore="includes/classes/pData.class.php" \
@@ -16,4 +18,11 @@ phpdoc -d "." -t "./docs" \
 --ignore="includes/classes/pIndicator.class.php" \
 --ignore="includes/classes/pCache.class.php" \
 --ignore="includes/classes/pSplit.class.php" \
---template="responsive-twig"
+--ignore="includes/apps/ant-app-default/tests/AntAppDefaultTest.php" \
+--ignore="includes/apps/ant-app-test-app/tests/AntAppClassTest.php" \
+--ignore="includes/apps/phpant-app-manager/tests/AntAppManagerTest.php" \
+--ignore="includes/apps/ant-app-authenticator/tests/UsersAndRolesTest.php" \
+--ignore="includes/apps/ant-app-authenticator/tests/APIAuthenticationTest.php" \
+--ignore="includes/apps/ant-app-authenticator/tests/UserPassAuthenticationTest.php" \
+--ignore="includes/apps/ant-app-authenticator/tests/RequestSelectorTest.php" \
+--ignore="includes/apps/ant-app-authenticator/tests/KeyAuthenticationTest.php"
