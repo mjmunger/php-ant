d202a72a1cce70b4d70af45426108170f2223160
Michael Munger <michael@highpoweredhelp.com>
2017-09-05 21:02:06 -0400

    Added JSON error exception

--- includes/classes/JSONConfigs.class.php
+++ includes/classes/JSONConfigs.class.php
@@ -18,6 +18,9 @@ class JSONConfigs extends BaseSetupConfigs
         $buffer = file_get_contents($configPath);
         $json   = json_decode($buffer);
         $this->jsonError = json_last_error();
+
+        if($this->jsonError !== JSON_ERROR_NONE) throw new Exception("JSON parsing error! The file $configPath generated the following error: " . json_last_error_msg() . PHP_EOL);
+
         $this->jsonErrorMsg = json_last_error_msg();
 
         $this->configs = $json;
